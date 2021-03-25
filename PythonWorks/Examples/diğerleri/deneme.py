00edde9f52d2ccd98bdff6003bbe244c0434241c
egemensahiin <egemensahin95@gmail.com>
2021-03-26 01:27:36 +0300

    New python examples at deneme.py

--- PythonWorks/Examples/diğerleri/deneme.py
+++ PythonWorks/Examples/diğerleri/deneme.py
@@ -1009,3 +1009,43 @@ random.shuffle(dental_health_kit)
 for action in dental_health_kit:
     actions.append(action.use())
 print(actions)
+
+print()
+
+# Exercise 'Creating Mocks'
+from unittest.mock import Mock
+
+# Let's mock a fake Airport object.
+# Create a Mock object and assign it to a variable called 'airport'. 
+
+# The airport mock should have a 'gates' attribute set to a list of the strings “A1”, “B2”, and “C3”.
+
+# The airport mock should have a 'departures' attribute set to a dictionary where 
+# the keys are strings representing cities and the 
+# values are strings representating their departure times.
+
+# {
+#   "Atlanta": "12:00PM",
+#   "Nashville": "04:30PM"
+# }
+
+# The airport mock should have a 'close' attribute that is callable (i.e. an instance method). 
+# When invoked, it should return the string “Closing”.
+
+# The airport should have an 'open' attribute that is callable (i.e. an instance method). .
+# When invoked the first time, it should return “Opening…”. 
+# When invoked the second time, it should return “Already open”.
+
+airport = Mock()
+airport.configure_mock(
+    gates = ["A1", "B2", "C3"],
+    departures = { "Atlanta": "12:00PM", "Nashville": "04:30PM" }
+    )
+airport.close.return_value = "Closing"
+airport.open.side_effect = ["Opening...", "Already open"]
+
+print(airport.gates)      # ["A1", "B2", "C3"]
+print(airport.departures) # { "Atlanta": "12:00PM", "Nashville": "04:30PM" }
+print(airport.close())    # Closing
+print(airport.open())     # Opening...
+print(airport.open())     # Already open
